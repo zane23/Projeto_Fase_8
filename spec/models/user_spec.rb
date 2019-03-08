0271d49e79ee86162e72c2ce0e4b412022b31284
andre-tlima <andre-tlima@hotmail.com>
2019-03-08 16:39:31 +0000

    Adiocionando testes com Shoulda Matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:user) { build(:user)}
+
+    it {is_expected.to validate_presence_of(:email)}
+    it {is_expected.to validate_uniqueness_of(:email).case_insensitive}
+    it {is_expected.to validate_confirmation_of(:password)}
+    it {is_expected.to allow_value("teste@supergeeks.com.br").for(:email)}
+    
 end
