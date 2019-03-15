f2795a4c96e74a1847ef5e9433219e408e1ef219
andre-tlima <andre-tlima@hotmail.com>
2019-03-15 16:49:09 +0000

    Adicionando teste para criação e exibição dos usuários

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do
+    let!(:user) {create(:user)}
+    let(:user_id) {user.id}
+
+    before {host! "localhost:3000/api"}
+
+    describe "GET user/:id" do
+        before do
+            headers = {"Accept" => "application/vnd.projetofase8.v1"}
+            get "/users/#{user_id}", params: {}, headers: headers
+        end
+
+        context "when the user exists" do
+            it "returns the user" do
+                user_response = JSON.parse(response.body)
+                expect(user_response["id"]).to eq(user_id)
+            end
+            it "returns status code 200" do
+                expect(response).to have_http_status(200)
+            end
+           
+        end
+
+        context "When user not exists" do
+            let(:user_id) {10000}
+            it "return status code 404" do
+                expect(response).to have_http_status(404)
+            end
+        
+        end
+        
+    end
+
+    describe "POST user/" do
+        before do
+            headers = {"Accept" => "application/vnd.projetofase8.v1"}
+            post "/users/", params:{user: user_params}, headers: headers
+        end
+
+        context "When the request params are valid" do
+            let(:user_params){ attributes_for(:user)}
+
+            it "returns status code 201" do
+                expect(response).to have_http_status(201)
+            end
+            it "returns json data for the created user" do
+                user_response = JSON.parse(response.body)
+                expect(user_response['email']).to eq(user_params[:email])
+            end
+        end
+
+        context "When the request params are invalid" do
+            let(:user_params){ attributes_for(:user, email: "email_invalido@")}
+
+            it "returns status code 422" do
+                expect(response).to have_http_status(422)
+            end
+
+            it "returns the json data for the errors" do
+                user_response = JSON.parse(response.body)
+                expect(user_response).to have_key('errors')
+            end
+
+        end
+    end
+end
