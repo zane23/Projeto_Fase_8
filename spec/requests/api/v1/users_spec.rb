80cb3868fea9d8f03a6703cb233a5b0a60d85e2e
andre-tlima <andre-tlima@hotmail.com>
2019-03-22 17:09:09 +0000

    Adicionando todos os testes de requisição

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -3,19 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Users API', type: :request do
     let!(:user) {create(:user)}
     let(:user_id) {user.id}
-    let(:headers){{"Accept" => "application/vnd.projetofase8.v1"}} #variavel que elimina os headers
+    let(:headers){{"Accept" => "application/vnd.projetofase8.v1"}}
     
     before {host! "localhost:3000/api"}
 
     describe "GET user/:id" do
         before do
-            #headers = {"Accept" => "application/vnd.projetofase8.v1"} 
+             
             get "/users/#{user_id}", params: {}, headers: headers
         end
 
         context "when the user exists" do
             it "returns the user" do
-                #user_response = JSON.parse(response.body)
                 expect(json_body["id"]).to eq(user_id)
             end
             it "returns status code 200" do
@@ -36,7 +35,6 @@ RSpec.describe 'Users API', type: :request do
 
     describe "POST user/" do
         before do
-            #headers = {"Accept" => "application/vnd.projetofase8.v1"}
             post "/users/", params:{user: user_params}, headers: headers
         end
 
@@ -64,6 +62,53 @@ RSpec.describe 'Users API', type: :request do
                 expect(json_body).to have_key('errors')
             end
 
+        end
+    end
+
+    describe "PUT user/:id" do
+        before do 
+            put "/users/#{user_id}", params:{user: user_params}, headers: headers
+        end
+
+        context "when the request params are valid" do
+            let(:user_params){{email: 'novo@email.com'}}
+
+            it "returns status code 200" do
+                expect(response).to have_http_status(200)
+            end
+
+            it "returns json data for the update user" do
+                expect(json_body['email']).to eq(user_params[:email])
+            end
+        end
+
+        context "when the request params are invalid" do
+            let(:user_params){{email: 'email_invalido@'}}
+
+            it "return status code 422" do
+                expect(response).to have_http_status(422)
+            end
+
+            it "return json data for the errors" do
+                expect(json_body).to have_key('errors')
+            end
+
+        end
+
+
+    end
+
+    describe "DELETE user/:id" do
+        before do
+            delete "/users/#{user_id}", params:{}, headers: headers
+        end
+
+        it "returns status code 204" do
+            expect(response).to have_http_status(204)
+        end
+
+        it "removes user from the database" do
+            expect(User.find_by(id: user.id)).to be_nil
         end
     end
 end
