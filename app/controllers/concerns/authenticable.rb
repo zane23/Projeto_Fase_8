638a5137c2ed9b2bffc800e0969822b120b1f91f
andre-tlima <andre-tlima@hotmail.com>
2019-03-29 19:24:11 +0000

    teste

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,10 @@
+module Authenticable
+    def current_user #
+        @current_user ||= User.find_by(auth_token: request.headers["Authorization"]) #pegar usuário atual
+    end
+
+    def authenticate_with_token!
+        render json: {errors: "Accesso não autorizado!"}, status: 401 unless current_user.present? #para qualquer outro usuario não acessar partes nao autorizadas.
+        
+    end
+end
